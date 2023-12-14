93c65492f19482309ce19d548f5f6fcabcbb4adf
Nikkqr <144231839+Nikkqr@users.noreply.github.com>
2023-12-14 12:27:32 +0300

    Errors changed test_circle.py

--- test_circle.py
+++ test_circle.py
@@ -5,28 +5,24 @@ from circle import perimeter
 class TestCircle(unittest.TestCase):
     
     def test_circle_area_correct(self):
-        res = area(6)
-        self.assertEqual(res, 113.09733552923255)
+        self.assertEqual(area(3), math.pi * 3 * 3)
         
     def test_circle_area_wrong(self):
-        res = area(-34)
-        self.assertRaises(res, Exception)
+        self.assertRaises(Exception, area, -2)
+        self.assertRaises(Exception, area, -1)
 
     def test_circle_area_zero(self):
-        res = area(0)
-        self.assertEqual(res, 0)
+        self.assertEqual(perimeter(0), 0)
 
     def test_circle_perimeter_correct(self):
-        res = perimeter(946)
-        self.assertEqual(res, 5943.893300591889)
+        self.assertEqual(perimeter(3), 2 * math.pi * 3)
 
     def test_circle_perimeter_wrong(self):
-        res = perimeter(-245)
-        self.assertRaises(res, Exception)
+        self.assertRaises(ValueError, perimeter, -2)
+        self.assertRaises(ValueError, perimeter, -1)
         
     def test_circle_perimeter_zero(self):
-        res = perimeter(0)
-        self.assertEqual(res, 0)
+        self.assertEqual(perimeter(0), 0)
         
 if __name__ == '__main__':
     unittest.main()
