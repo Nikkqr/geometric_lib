da1a936fa399d9b99f36cd45024dd08a79404f76
Nikkqr <144231839+Nikkqr@users.noreply.github.com>
2023-12-14 12:34:00 +0300

    errors changed in test_rectangle.py

--- test_rectangle.py
+++ test_rectangle.py
@@ -5,27 +5,22 @@ from rectangle import perimeter
 class RectangleTestCase(unittest.TestCase):
 
     def test_rectangle_area_correct(self):
-        res = area(12, 15)
-        self.assertEqual(res, 180)
+        self.assertEqual(area(2, 3), 2 * 3)
 
     def test_rectangle_area_wrong(self):
-        res = area(-6, 3)
-        self.assertRaises(res, Exception)
+        self.assertRaises(Exception, area, -2, 2)
 
     def test_rectangle_area_null(self):
-        res = area(10, 0)
-        self.assertEqual(res, 0)
+        self.assertEqual(area(0, 0), 0)
 
     def test_rectangle_perimeter_correct(self):
-        res = perimeter(42, 36)
-        self.assertEqual(res, 156)
+        self.assertEqual(perimeter(2, 3), (2 + 3) * 2)
 
     def test_rectangle_perimeter_wrong(self):
-        self.assertRaises(res, Exception)
+        self.assertRaises(Exception, area, -2, 2)
 
-    def test_rectangle_perimeter_null(self):
-        res = perimeter(0, 35567)
-        self.assertEqual(res, 71134)
+    def test_rectangle_perimeter_symbol(self):
+        self.assertRaises(Exception, area, 'one', 2)
         
 if __name__ == '__main__':
     unittest.main()
