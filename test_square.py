d012b1817dbd2803ea065f5ba17d22b4c1fe0e27
Nikkqr <144231839+Nikkqr@users.noreply.github.com>
2023-12-14 12:37:32 +0300

    errors changed in test_square.py

--- test_square.py
+++ test_square.py
@@ -5,28 +5,22 @@ from square import perimeter
 class TestSquare(unittest.TestCase):
     
     def test_square_area_correct(self):
-        res = area(12)
-        self.assertEqual(res, 144)
+        self.assertEqual(area(2), 2 * 2)
 
     def test_square_area_wrong(self):
-        res = area(-98)
-        self.assertRaises(res, Exception)
+        self.assertRaises(Exception, area, -2)
 
     def test_square_area_null(self):
-        res = area(0)
-        self.assertEqual(res, 0)
+        self.assertEqual(area(0), 0)
 
     def test_square_perimeter_correct(self):
-        res = perimeter(42)
-        self.assertEqual(res, 168)
+        self.assertEqual(perimeter(2), 4 * 2)
 
     def test_square_perimter_wrong(self):
-        res = perimeter(-245)
-        self.assertRaises(res, Exception)
+        self.assertRaises(Exception, perimeter, -2)
 
     def test_square_perimeter_null(self):
-        res = perimeter(0)
-        self.assertEqual(res, 0)    
+          self.assertEqual(perimeter(0), 0)
         
 if __name__ == '__main__':
     unittest.main()
